0f2c2a4b63dd0898670bf1344e28bb20a1a67dc7
Omri Katz <omri.katz@kaltura.com>
2020-04-21 17:39:32 +0300

    add test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ const Header = ({ siteTitle }) => (
                         textDecoration: `none`,
                     }}
                 >
-                    {siteTitle}
+                    {siteTitle} Test
                 </Link>
             </h1>
         </div>
